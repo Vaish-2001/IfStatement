0a12a95bd7aaafb5d33c49311191985114fa16c2
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-03-01 19:31:57 +0530

    Add files via upload

---
+++ IfStatement.java
@@ -0,0 +1,11 @@
+class IfStatement
+ {
+   public static void main(String[]args)
+    {
+     int age=20;
+     if(age>=18);
+      {
+        System.out.println("congrats! you are eligible for voting");
+      }
+    }
+ }
